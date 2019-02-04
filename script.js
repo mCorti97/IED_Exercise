b823502b7dd4b0621f413cac969874074570d809
ieduser <ieduser@fi-lab10-03.ied.edu>
2019-02-04 12:22:06 +0100

    Updating JS

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-prompt("Write a number:");
+
+ var myNumber = prompt("Write a number:");
+ myNumber = parseInt(myNumber);
+
+ if ( myNumber < 0){
+     alert('Given number is negative, Absing it..');
+     myNumber = Math.abs(myNumber);
+ }else{
+     alert("Given number is positive");
+ }
+
+ var squareRoot = Math.sqrt(myNumber);
+
+ alert(squareRoot);
